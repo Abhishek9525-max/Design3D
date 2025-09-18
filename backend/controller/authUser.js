cc45b1c559d4f784308d0062942933c316831eac
Abhishek <abhisheksaxena9525@gmail.com>
2025-09-18 11:16:30 +0530

    MVC

---
+++ backend/controller/authUser.js
@@ -0,0 +1,93 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// @route POST /api/auth/register
+// @desc Register a new user
+// @access Public
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    //Registration Logic
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: " User already exits" });
+
+    user = new User({ name, email, password });
+    await user.save();
+
+    //Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // sign and return the token along with user data
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //Send the user and token in response
+        res.status(201).json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// @route POST /api/auth/login
+// @desc Authenticate user
+// @access Public
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //find the user by email
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid Email" });
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+    const payload = { user: { id: user._id, role: user.role } };
+
+    //sign and return the token along with user data
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //Send the user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = { register, login };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/Product");
-const {protect} = require('../middleware/authMiddleware')
+const {protect} = require('../middleware/authMiddleware');
+const { product, products } = require("../controller/product");
 
 // GET all products
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get("/", products);
 
 // GET single product by ID
-router.get("/:id",protect, async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) return res.status(404).json({ message: "Not found" });
-    res.json(product);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get("/:id",protect, product);
 
 module.exports = router;
